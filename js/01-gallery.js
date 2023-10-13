7aada34da51aefac17dfe49ab8afa6b67a49024c
Iryna Holova <gis.golova@gmail.com>
2023-10-13 14:14:48 +0300

    add scripts

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,45 @@
-import { galleryItems } from "./gallery-items.js";
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector('.gallery');
+gallery.addEventListener('click', handleGalleryItemClick);
 
-console.log(galleryItems);
+const galleryMarkup = galleryItems.map(makeGalleryItemMarkup).join('');
+gallery.insertAdjacentHTML('beforeend', galleryMarkup);
+
+let modal;
+
+function makeGalleryItemMarkup({ preview, original, description }) {
+  return `<li class="gallery__item">
+            <a class="gallery__link" href=${original}>
+              <img
+                class="gallery__image"
+                src=${preview}
+                data-source=${original}
+                alt='${description}'
+              />
+            </a>
+          </li>`;
+}
+
+function handleGalleryItemClick(e) {
+  if (e.target.nodeName !== 'IMG') return;
+
+  e.preventDefault();
+
+  const imageSrc = e.target.dataset.source;
+  const imageAlt = e.target.getAttribute('alt');
+
+  onModalOpen(imageSrc, imageAlt);
+}
+
+function onModalOpen(src, alt) {
+  modal = basicLightbox.create(`<img src=${src} alt=${alt}>`);
+  modal.show();
+  window.addEventListener('keydown', handleModalClose);
+}
+
+function handleModalClose(e) {
+  if (e.code !== 'Escape') return;
+  modal.close();
+  window.removeEventListener('keydown', handleModalClose);
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,17 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector('.gallery');
 
-console.log(galleryItems);
+const galleryMarkup = galleryItems.map(makeGalleryItemMarkup).join('');
+gallery.insertAdjacentHTML('beforeend', galleryMarkup);
+
+let galleryBox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
+galleryBox.on('show.simplelightbox');
+
+function makeGalleryItemMarkup({ preview, original, description }) {
+  return `<li class="gallery__item">
+            <a class="gallery__link" href=${original}>
+              <img class="gallery__image" src=${preview} alt='${description}' />
+            </a>
+          </li>`;
+}
