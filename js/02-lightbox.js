e6856e146c77ed0986da44b06e67ab024db35e73
Iryna Holova <gis.golova@gmail.com>
2023-10-22 03:05:29 +0300

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -23,8 +23,8 @@ new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
  */
 function makeGalleryItemMarkup({ preview, original, description }) {
   return `<li class="gallery__item">
-            <a class="gallery__link" href=${original}>
-              <img class="gallery__image" src=${preview} alt='${description}' />
+            <a class="gallery__link" href="${original}">
+              <img class="gallery__image" src="${preview}" alt="${description}" />
             </a>
           </li>`;
 }
